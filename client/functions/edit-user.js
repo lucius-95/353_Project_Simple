732c0d6228b1ccb2b1a272c7a6dee91b3aa34a47
lucius-95 <77852558+lucius-95@users.noreply.github.com>
2021-11-21 19:48:38 -0600

    updated customer.html

--- client/functions/edit-user.js
+++ client/functions/edit-user.js
@@ -1,23 +1,32 @@
 function editUser(id) {
-	if (document.getElementById(id) === null) {
+	if (!id) {
+		alert("ERROR: Please enter an ID number!");
+	} else if (document.getElementById(id) === null) {
 		alert(`ERROR: Could not find any user with ID ${id}!`);
 	} else {
-		let http = new XMLHttpRequest();
 		let firstname = document.getElementById("newFirstname").value;
 		let lastname = document.getElementById("newLastname").value;
-		let params =
-			"id=" + id + "&firstname=" + firstname + "&lastname=" + lastname;
+		if (!(firstname && lastname)) {
+			alert("ERROR: First name and/or last name must not be empty!");
+		} else {
+			let http = new XMLHttpRequest();
+			let params =
+				"id=" + id + "&firstname=" + firstname + "&lastname=" + lastname;
 
-		http.open("POST", "/edit-user", true);
-		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		http.send(params);
+			http.open("POST", "/edit-user", true);
+			http.setRequestHeader(
+				"Content-type",
+				"application/x-www-form-urlencoded"
+			);
+			http.send(params);
 
-		http.onload = () => {
-			if (http.status === 200) {
-				alert("User edited successfully!");
-			} else {
-				alert("ERROR: Failed to edit user!");
-			}
-		};
+			http.onload = () => {
+				if (http.status === 200) {
+					alert("Successfully updated user!");
+				} else {
+					alert("ERROR: Failed to update user!");
+				}
+			};
+		}
 	}
 }
